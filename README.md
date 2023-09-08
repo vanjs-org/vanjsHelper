21f8293a395e031799946430e3462d3fa2455a54
Tao Xin <tao@vanjs.org>
2023-09-08 10:51:05 -0700

    Initial commit for the VS Code extension

---
+++ README.md
@@ -0,0 +1,15 @@
+# vanjsHelper README
+
+This is my personal helper for writing **VanJS** and **Mini-Van** code. Specifically, it defines commands that are helpful for building the https://vanjs.org/ website.
+
+This extension is only intended for my personal use, as the commands are very specific to my use cases. That said, I feel that open sourcing it might be helpful if it can inspire people to build similar extensions.
+
+## Command Reference
+
+* `Insert Child` (`vanjsHelper.insertChild`): Shortcut: `ctrl+8`. Inserts a new child node in the current tag.
+
+* `Insert Link` (`vanjsHelper.insertLink`): Shortcut: `ctrl+7`. Inserts a `Link` component.
+
+* `Insert Symbol` (`vanjsHelper.insertSymbol`): Shortcut: `ctrl+6`. Inserts a `Symbol` component.
+
+* `vanjsHelper: Import Component` (`vanjsHelper.importComponent`): Add import for the component at the cursor defined in [`common.ts`](https://github.com/vanjs-org/vanjs-org.github.io/blob/master/sitegen/common.ts).
---
+++ src/extension.ts
@@ -0,0 +1,71 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+const withEditor = (f: (editor: vscode.TextEditor) => void) => () => {
+	const editor = vscode.window.activeTextEditor
+	if (editor) f(editor)
+}
+
+const move = (editor: vscode.TextEditor, num: number) => {
+	const newPos = editor.selection.active.translate(0, num)
+	editor.selection = new vscode.Selection(newPos, newPos)
+}
+
+const getWordAtPosition = (editor: vscode.TextEditor) => {
+	const wordRange = editor.document.getWordRangeAtPosition(editor.selection.start)
+	if (!wordRange) return
+	return editor.document.getText(wordRange);
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "vanjsHelper" is now active!');
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vanjsHelper.insertSymbol', withEditor(editor => {
+			editor.insertSnippet(new vscode.SnippetString('Symbol("")'))
+			move(editor, 8)
+		})),
+		vscode.commands.registerCommand('vanjsHelper.insertLink', withEditor(async editor => {
+			editor.insertSnippet(new vscode.SnippetString('Link("", "")'))
+			move(editor, 6)
+		})),
+		vscode.commands.registerCommand('vanjsHelper.insertChild', withEditor(async editor => {
+			editor.insertSnippet(new vscode.SnippetString('", , " '))
+			move(editor, 3)
+		})),
+		vscode.commands.registerCommand('vanjsHelper.importComponent', withEditor(async editor => {
+			const word = getWordAtPosition(editor)
+			if (!word) {
+				vscode.window.showErrorMessage("No symbol at the current point")
+				return
+			}
+
+			const doc = editor.document
+			const regex = /{([\w\s,]*)}\s=\scommon\(doc\)/
+			let matches: RegExpMatchArray | null = null
+			const tmpl = "{$COMPONENTS} = common(doc)"
+			for (let i = 0; !matches && i < doc.lineCount; ++i) {
+				const {text} = doc.lineAt(i)
+				matches = text.match(regex)
+				if (!matches) continue
+				const start = matches.index!, end = matches.index! + matches[0].length
+				const components = matches[1] ? matches[1].split(/\s*,\s*/) : []
+				editor.edit(editorBuilder => editorBuilder.replace(
+					new vscode.Range(new vscode.Position(i, start), new vscode.Position(i, end)),
+					tmpl.replace("$COMPONENTS", [...new Set([...components, word])].sort().join(", "))
+				))
+			}
+
+			if (!matches) vscode.window.showErrorMessage("No component import line found.")
+		})),
+	);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
